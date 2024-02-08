f7d95f3c33ea59ce480d16043296acbd6769c71d
Daniel Szatmari <daniel.kaizencodes@gmail.com>
2024-02-08 14:07:37 +0000

    scaffold for Message
    generates scaffold for messages resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :messages
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -9,7 +10,7 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  root to: 'home#index'
+  root to: 'messages#index'
 
-  get '/', to: 'home#index'
+  get '/', to: 'messages#index'
 end
