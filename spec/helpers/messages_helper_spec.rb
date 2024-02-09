c7e928dd0a86832ef8b3541f759c03bab8a07898
Daniel Szatmari <daniel.kaizencodes@gmail.com>
2024-02-09 09:30:38 +0000

    revert deletion check
    There is an issue with devise and turbolink, the current_user cannot be used
    https://discuss.hotwired.dev/t/authentication-and-devise-with-broadcasts/1752/4
    https://blog.cloud66.com/making-hotwire-and-devise-play-nicely-with-viewcomponents

--- spec/helpers/messages_helper_spec.rb
+++ spec/helpers/messages_helper_spec.rb
@@ -2,29 +2,16 @@
 
 require 'rails_helper'
 
+# Specs in this file have access to a helper object that includes
+# the MessagesHelper. For example:
+#
+# describe MessagesHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
 RSpec.describe MessagesHelper, type: :helper do
-  describe '#message_belongs_to_current_user?' do
-    let(:user) { create(:user) }
-    let(:message) { create(:message, user:) }
-
-    context 'when the message belongs to the current user' do
-      before do
-        allow(helper).to receive(:current_user).and_return(user)
-      end
-
-      it 'returns true' do
-        expect(helper.message_belongs_to_current_user?(message)).to eq(true)
-      end
-    end
-
-    context 'when the message does not belong to the current user' do
-      before do
-        allow(helper).to receive(:current_user).and_return(create(:user))
-      end
-
-      it 'returns false' do
-        expect(helper.message_belongs_to_current_user?(message)).to eq(false)
-      end
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -12,20 +12,10 @@ RSpec.describe '/rooms/room_id/messages', type: :request do
 
   describe 'GET /edit' do
     it 'renders a successful response' do
-      message = create(:message, room:, user:)
+      message = create(:message, room:)
       get edit_room_message_url(room, message)
       expect(response).to have_http_status(:success)
       expect(response.body).to include(message.content)
-    end
-
-    context 'when the message does not belong to the current user' do
-      it 'redirects to the room page' do
-        message = create(:message, room:)
-
-        get edit_room_message_url(room, message)
-
-        expect(response).to redirect_to(room_url(room))
-      end
     end
   end
 
@@ -60,7 +50,7 @@ RSpec.describe '/rooms/room_id/messages', type: :request do
   describe 'PATCH /update' do
     context 'with valid parameters' do
       it 'updates the requested message' do
-        message = create(:message, content: 'original content', room:, user:)
+        message = create(:message, content: 'original content', room:)
 
         patch room_message_url(room, message), params: { message: { content: 'new content' } }
 
@@ -69,7 +59,7 @@ RSpec.describe '/rooms/room_id/messages', type: :request do
       end
 
       it 'redirects to the room page' do
-        message = create(:message, content: 'original content', user:)
+        message = create(:message, content: 'original content')
 
         patch room_message_url(room, message), params: { message: { content: 'new content' } }
 
@@ -77,22 +67,14 @@ RSpec.describe '/rooms/room_id/messages', type: :request do
       end
     end
 
-    context 'when the message does not belong to the current user' do
-      it 'does not update the message' do
-        message = create(:message, content: 'original content', room:)
-        patch room_message_url(room, message), params: { message: { content: 'new content' } }
-        message.reload
-        expect(message.content).to eq 'original content'
-      end
-    end
-
     context 'with invalid parameters' do
       it 'does not update the message' do
         message = create(:message, content: 'original content')
 
-        expect do
-          patch room_message_url(room, message), params: { message: { foo: 'bar' } }
-        end.to_not change(message, :content)
+        patch room_message_url(room, message), params: { message: { foo: 'bar' } }
+
+        message.reload
+        expect(message.content).to eq 'original content'
       end
 
       it 'redirects to the room page' do
@@ -107,7 +89,7 @@ RSpec.describe '/rooms/room_id/messages', type: :request do
 
   describe 'DELETE /destroy' do
     it 'destroys the requested message' do
-      message = create(:message, room:, user:)
+      message = create(:message, room:)
 
       expect do
         delete room_message_url(room, message)
@@ -115,21 +97,11 @@ RSpec.describe '/rooms/room_id/messages', type: :request do
     end
 
     it 'redirects to the room page' do
-      message = create(:message, room:, user:)
+      message = create(:message, room:)
 
       delete room_message_url(room, message)
 
       expect(response).to redirect_to(room_url(room))
-    end
-
-    context 'when the message does not belong to the current user' do
-      it 'does not destroy the message' do
-        message = create(:message, room:)
-
-        expect do
-          delete room_message_url(room, message)
-        end.to_not change(room.messages, :count)
-      end
     end
   end
 end
