c03e70c5ff7aa8d50f13cf2e14890275bc19865b
Daniel Szatmari <daniel.kaizencodes@gmail.com>
2024-02-09 08:30:53 +0000

    Improve UI

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,5 @@ pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin "local-time" # @3.0.2
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "@popperjs/core", to: "popper.js", preload: true
